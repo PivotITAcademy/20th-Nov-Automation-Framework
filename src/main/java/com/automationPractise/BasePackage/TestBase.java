2f44ae6d2c6407e43efe72512aba5348ea139a8a
Manvir <manvir.singh.parmar@gmail.com>
2022-04-02 13:35:17 -0400

    Added Listeners classes

--- src/main/java/com/automationPractise/BasePackage/TestBase.java
+++ src/main/java/com/automationPractise/BasePackage/TestBase.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.BeforeClass;
+
+import com.automationPractise.Logger.WebdriverEvents;
+import com.automationPractise.Utils.Utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,6 +26,9 @@ public class TestBase {
 	public static WebDriver wd;
 	public static Properties prop;
 	public FileInputStream file;
+	public static Logger logger;
+	public EventFiringWebDriver e_driver;
+	public WebdriverEvents eventListner;
 
 	public TestBase() {
 		try {
@@ -29,6 +41,16 @@ public class TestBase {
 
 			e.printStackTrace();
 		}
+	}
+
+	@BeforeClass
+	public void setUpLogger() {
+		logger = Logger.getLogger(TestBase.class);
+		PropertyConfigurator.configure("log4j.properties");
+		BasicConfigurator.configure();
+
+		logger.setLevel(Level.INFO);
+
 	}
 
 	public void intialsation() {
@@ -52,10 +74,16 @@ public class TestBase {
 			System.out.println("You are not passing the correct browser name!!!!!");
 			break;
 		}
+		
+		//Intialising Webdriver Event Listener
+		e_driver = new EventFiringWebDriver(wd);
+		eventListner = new WebdriverEvents();
+		e_driver.register(eventListner);
+		wd = e_driver;
 
 		wd.manage().window().maximize();
-		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		wd.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		wd.manage().timeouts().implicitlyWait(Utils.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		wd.manage().timeouts().pageLoadTimeout(Utils.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 
 		// Load the Page
 		wd.get(prop.getProperty("URL"));
---
+++ src/main/java/com/automationPractise/retryLogic/RetryFailedTestCases.java
@@ -0,0 +1,28 @@
+package com.automationPractise.retryLogic;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import com.automationPractise.BasePackage.TestBase;
+
+public class RetryFailedTestCases extends TestBase implements IRetryAnalyzer {
+
+	
+	int counter=1; // Intial value
+	int maxCount=3; //maximum retry count
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		
+		logger.info("Trying failed Test Case " + counter +" times");
+		
+		if(counter<maxCount) {
+			counter++;
+			return true;
+		}
+		
+		
+		return false;
+	}
+
+}
